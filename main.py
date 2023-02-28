74882fa2fa6d5a44af4f2ac5529b2e406a48ce26
armandfardeau <fardeauarmand@gmail.com>
2023-02-28 12:27:16 +0100

    Rename label to message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def predict():
-    print(request.get_json())
-    return {"label": "NEG"}
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return {"message": "pong"}
