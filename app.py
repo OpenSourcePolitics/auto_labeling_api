ee78e55f8252072e25062216880427eb9f6ae737
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-02-28 19:34:13 +0100

    Use only french (#1)

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from utils.config import cache, config
 from utils.type_helper import get_labels
 from markupsafe import escape
 from pipelines.classification import classify_sequence
-from utils.translations_helper import translate
 
 app = Flask(__name__)
 cache.init_app(app, config=config)
@@ -23,12 +22,9 @@ def get_classify(type):
 
 
 @cache.cached()
-@app.route('/classify/<type>', defaults={'lang': "EN"}, methods=["POST"])
-@app.route("/classify/<type>/<lang>", methods=["POST"])
-def post_classify(type, lang):
-    lang = escape(lang).striptags().upper()
+@app.route("/classify/<type>", methods=["POST"])
+def post_classify(type):
     type = escape(type).striptags().lower()
     labels = get_labels(type)
-    old_sequence = request.get_json()["sequence"]
-    sequence = translate(old_sequence, lang)
-    return dict(classify_sequence(sequence, labels), **{"old_sequence": old_sequence, "lang": lang, "type": type})
+    sequence = request.get_json()["sequence"]
+    return dict(classify_sequence(sequence, labels), **{"type": type})
